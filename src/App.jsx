9e5c2d7c56ea587d2bd092855b21cb6132b10701
Digitalplanets <108709895+Digitalplanets@users.noreply.github.com>
2023-07-04 02:26:50 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Home from './pages/Home';
 import PageReveal from './pages/PageReveal';
 import Expertise from './pages/Expertise';
 import Booking from './pages/Booking';
+import Education from './pages/Education';
+import HighLevelConversation from './pages/HighLevelConversation';
+
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<PageReveal />} />
       <Route path='/home' element={<Home />} />
+      <Route path='/education' element={<Education />} />
+      <Route path='/high-level-conversations' element={<HighLevelConversation />} />
       <Route path='/expertise' element={<Expertise />} />
       <Route path='/booking' element={<Booking />} />
     </Routes>
